fc2836c9d5ff6f975f6e9d44cf3d492386289d97
naufal <naufalrandialdiansyah@gmail.com>
2021-04-14 11:43:15 +0700

    update edit function

--- src/Controllers/LowonganController.php
+++ src/Controllers/LowonganController.php
@@ -8,7 +8,6 @@ use Bageur\Karir\Model\karir_tag;
 
 class LowonganController extends Controller
 {
-
     public function index(Request $request)
     {
         $query =  karir::query();
@@ -50,6 +49,70 @@ class LowonganController extends Controller
             return ['status' => false , 'err' => $err];
         }else{
             $input                      = new karir;
+            $input->perusahaan_id       = $request->perusahaan;
+            $input->judul               = $request->judul;
+            $input->judul_seo           = \Str::slug($request->judul,'-');
+            $input->posisi              = $request->posisi;
+            $input->tags                = json_encode($request->tags);
+            $input->type_pekerjaan      = $request->type_pekerjaan;
+            $input->fresh_graduate      = $request->fresh_graduate;
+            $input->pengalaman_mulai    = $request->pengalaman_mulai;
+            $input->pengalaman_selesai  = $request->pengalaman_selesai;
+            $input->remote              = $request->remote;
+            $input->matauang            = $request->matauang;
+            $input->kualifikasi         = json_encode($request->kualifikasi);
+            $input->skill               = json_encode($request->skill);
+            $input->tugas               = json_encode($request->tugas);
+            $input->gaji_max            = $request->gaji_max;
+            $input->gaji_min            = $request->gaji_min;
+            $input->tanggal_mulai       = $request->tanggal_mulai;
+            $input->tanggal_selesai     = $request->tanggal_selesai;
+            $input->save();
+            foreach ($request->tags as $value) {
+                karir_tag::firstOrCreate(['tag' => $value]);
+            }
+            return ['status' => true];
+        }
+    }
+    public function show($id)
+    {
+        $query =  karir::where('user_id',\Auth::guard('api_mobile')->id())->find($id);
+        $query['tags']            = json_decode($query->tags);
+        return $query;
+    }
+
+    public function update(Request $request,$id)
+    {
+        $rules = [
+            'perusahaan'        => 'required',
+            'judul'             => 'required',
+            'posisi'            => 'required',
+            'tags'              => 'required',
+            'type_pekerjaan'    => 'required',
+            'remote'            => 'required',
+            'matauang'          => 'required',
+            'kualifikasi'       => 'required',
+            'skill'             => 'required',
+            'tugas'             => 'required',
+            'gaji_max'          => 'required|numeric',
+            'gaji_min'          => 'required|numeric',
+            'tanggal_mulai'     => 'required|date',
+            'tanggal_selesai'   => 'required|date',
+        ];
+        $validator = \Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            $errors = $validator->errors();
+            $err = [];
+            foreach ($rules as $key => $value) {
+                if ($errors->has($key)) {
+                    $err[$key] = ['text' => $errors->first($key) , 'status' => 'is-danger'];
+                }else{
+                    $err[$key] = ['text' => '' , 'status' => 'is-success'];
+                }
+            }
+            return ['status' => false , 'err' => $err];
+        }else{
+            $input                      = karir::find($id);
             $input->perusahaan_id       = $request->perusahaan;
             $input->judul               = $request->judul;
             $input->judul_seo           = \Str::slug($request->judul,'-');
--- src/Controllers/PerusahaanController.php
+++ src/Controllers/PerusahaanController.php
@@ -66,5 +66,60 @@ class PerusahaanController extends Controller
              return ['status' => true];
         }
     }
+    public function show($id)
+    {
+        $query =  perusahaan::where('user_id',Auth::guard('api_mobile')->id())->find($id);
+        return $query; 
+    }
+
+    public function update(Request $request,$id)
+    {
+        $rules = [
+            'foto'          => 'required',
+            'perusahaan'    => 'required',
+            'keterangan'    => 'required',
+            // 'email'         => 'required|email',
+            // 'notelp'        => 'required',
+            // 'nowa'          => 'required',
+            'provinsi'      => 'required',
+            'kota'          => 'required',
+            // 'alamat'        => 'required',
+            'web'           => 'nullable|url',
+            'fb'            => 'nullable|url',
+            'ig'            => 'nullable|url',
+            'linkedin'      => 'nullable|url',
+        ];
+        $validator = \Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            $errors = $validator->errors();
+            $err = [];
+            foreach ($rules as $key => $value) {
+                if ($errors->has($key)) {
+                    $err[$key] = ['text' => $errors->first($key) , 'status' => 'is-danger'];
+                }else{
+                    $err[$key] = ['text' => '' , 'status' => 'is-success'];
+                }
+            }
+            return ['status' => false , 'err' => $err];
+        }else{
+            $input              = perusahaan::find($id);
+            $input->user_id      = Auth::guard('api_mobile')->id();
+            $input->foto      = $request->foto;
+            $input->keterangan      = $request->keterangan;
+            $input->perusahaan      = $request->perusahaan;
+            $input->email       = $request->email;
+            $input->notelp      = $request->notelp;
+            $input->nowa        = $request->nowa;
+            $input->provinsi        = $request->provinsi;
+            $input->kota        = $request->kota;
+            $input->alamat      = $request->alamat;
+            $input->web         = $request->web;
+            $input->fb      = $request->fb;
+            $input->ig      = $request->ig;
+            $input->linkedin        = $request->linkedin;
+            $input->save();
+             return ['status' => true];
+        }
+    }
 
 }
